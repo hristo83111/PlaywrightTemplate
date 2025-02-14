6b4b2db66549f231a70e85c95558240ff3e9350a
hristo-georgiev_mcorp <hristo.georgiev@moneycorp.com>
2025-02-14 09:34:44 +0200

    Add command line variables to gitAction yml file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,14 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('has title', { tag: ['@regression', '@smoke'] }, async ({ page }) => {
   await page.goto('https://playwright.dev/')
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/)
+  console.log(process.env.ENVIRONMENT)
 })
 
-test('get started link', async ({ page }) => {
+test('get started link', { tag: ['@regression'] }, async ({ page }) => {
   await page.goto('https://playwright.dev/')
 
   // Click the get started link.
